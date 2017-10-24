a3983702db52557b9c8872a19ec2f76b43e7ccd1
puakai <puakai2010@gmail.com>
2017-10-25 00:23:57 +0900

    shitty huffman

--- OpenGl1_Editor/src/Compressors.cpp
+++ OpenGl1_Editor/src/Compressors.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
+#include <bitset>
 
 int IndexInDic (std::string& str, std::vector<std::string>& dic) {
     for (unsigned short x = dic.size(); x > 0; x--) {
@@ -129,4 +130,161 @@ void Compressors::Decompress_LZW(std::ifstream& strm, std::vector<unsigned char>
 		prevCode = currCode + 0;
 		dicSize++;
 	}
+}
+
+struct ch_node {
+	ch_node(byte b) : parent(0), node_l(0), node_r(0), val(b), weight(0) {}
+	ch_node(ch_node* a, ch_node* b) : parent(0), node_l(a), node_r(b), val(0), weight(a->weight + b->weight) {
+		a->parent = b->parent = this;
+	}
+
+	ch_node* parent, *node_l, *node_r;
+	byte val;
+	uint weight;
+};
+
+void ch_bittree(std::vector<bool>& bits, ch_node* node) {
+	if (node->node_l) {
+		bits.push_back(0);
+		ch_bittree(bits, node->node_l);
+		ch_bittree(bits, node->node_r);
+	}
+	else {
+		bits.push_back(1);
+		auto b = std::bitset<8>(node->val);
+		for (int i = 0; i < 8; i++) bits.push_back(b[i]);
+	}
+}
+void ch_bitval(std::vector<bool>& bits, ch_node* node, std::vector<ch_node*> tree) {
+	int x = 0;
+	while (node->parent) {
+		tree.push_back(node);
+		node = node->parent;
+		x++;
+	}
+	tree.push_back(node);
+	while (x > 0) {
+		bits.push_back(tree[x - 1] == tree[x]->node_r);
+		x--;
+	}
+	int i = 0;
+}
+
+std::vector<byte> Compressors::Compress_Huffman(byte* input, uint inSize) {
+	std::vector<ch_node*> nodes;
+	nodes.reserve(256);
+	for (uint i = 0; i < 256; i++) {
+		nodes.push_back(new ch_node(i));
+	}
+	for (uint i = 0; i < inSize; i++) {
+		nodes[input[i]]->weight++;
+	}
+	
+	auto nodes2(nodes);
+	for (int i = 255; i >= 0; i--) {
+		if (nodes2[i]->weight == 0) {
+			nodes2.erase(nodes2.begin() + i);
+		}
+	}
+	byte x = nodes2.size()-1;
+	while (x > 0) {
+		std::sort(nodes2.begin(), nodes2.end(), [](ch_node* a, ch_node* b) {
+			return (a->weight > b->weight);
+		});
+		ch_node* a = nodes2[x--];
+		ch_node* b = nodes2[x];
+		nodes2.pop_back();
+		nodes2[x] = new ch_node(a, b);
+	}
+
+	std::vector<bool> bitmaps[256];
+	byte bitmapSz[256];
+	for (uint i = 0; i < 256; i++) {
+		ch_bitval(bitmaps[i], nodes[i], std::vector<ch_node*>());
+		bitmapSz[i] = bitmaps[i].size();
+	}
+
+	std::vector<bool> bits;
+	bits.reserve(inSize*4);
+	ch_bittree(bits, nodes2[0]); //recursive tree generation
+	auto b = std::bitset<32>(inSize);
+	for (int i = 0; i < 32; i++) bits.push_back(b[i]);
+	for (uint i = 0; i < inSize; i++) { //characters
+		//bits.insert(bits.end(), bitmaps[input[i]].begin(), bitmaps[input[i]].end());
+	}
+	
+	uint sz = bits.size();
+	/*
+	byte pad = 8 - sz % 8;
+	if (pad == 8) pad = 0;
+	bits[0] = !!(pad & 4);
+	bits[1] = !!(pad & 2);
+	bits[2] = !!(pad & 1);
+	*/
+	std::vector<byte> res;
+	res.reserve(sz / 8 + inSize / 2 + 1);
+	byte bt = 0, off = 7;
+	for (uint i = 0; i < sz; i++) {
+		bt |= (((byte)bits[i]) << off--);
+		if (off == 255) {
+			res.push_back(bt);
+			bt = 0;
+			off = 7;
+		}
+	}
+	for (uint i = 0; i < inSize; i++) { //characters
+		for (byte j = 0; j < bitmapSz[input[i]]; j++) {
+			bt |= (((byte)bitmaps[input[i]][j]) << off--);
+			if (off == 255) {
+				res.push_back(bt);
+				bt = 0;
+				off = 7;
+			}
+		}
+	}
+	if (off != 7) res.push_back(bt);
+	return res;
+}
+
+void dh_bittree(std::vector<bool>& bits, uint& off, ch_node* me) {
+	if (bits[off++]) { //this is leaf
+		byte bt = 0;
+		for (byte b = 0; b < 8; b++)
+			bt |= ((byte)bits[off++] << b);
+		me->val = bt;
+	}
+	else { //this is node
+		me->node_l = new ch_node(0);
+		me->node_r = new ch_node(0);
+		dh_bittree(bits, off, me->node_l);
+		dh_bittree(bits, off, me->node_r);
+	}
+}
+byte dh_bitbyte(std::vector<bool>& bits, uint& off, ch_node* tree) {
+	while (tree->node_l) {
+		tree = (bits[off++] ? tree->node_r : tree->node_l);
+	}
+	return tree->val;
+}
+std::vector<byte> Compressors::Decompress_Huffman(byte* input, uint inSize) {
+	std::vector<bool> bits;
+	bits.reserve(inSize * 8);
+	for (uint a = 0; a < inSize; a++)
+		for (char b = 7; b >= 0; b--)
+			bits.push_back(!!(input[a] & (1 << b)));
+	uint off = 0;
+	ch_node* node = new ch_node(0);
+	dh_bittree(bits, off, node);
+	
+	uint num = 0;
+	for (byte a = 0; a < 32; a++) {
+		num |= bits[off++] << a;
+	}
+	std::vector<byte> res;
+	res.reserve(num);
+	while (num > 0) {
+		res.push_back(dh_bitbyte(bits, off, node));
+		num--;
+	}
+	return res;
 }
--- OpenGl1_Editor/src/Compressors.h
+++ OpenGl1_Editor/src/Compressors.h
@@ -1,9 +1,17 @@
+#ifndef COMPRESS_H
+#define COMPRESS_H
+
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include "Engine.h"
 
 class Compressors {
 public:
 	static void Compress_LZW(unsigned char* input, unsigned int inSize, std::vector<unsigned char>& output);
 	static void Decompress_LZW(std::ifstream& strm, std::vector<unsigned char>& output);
+	static std::vector<byte> Compress_Huffman(byte* input, uint inSize);
+	static std::vector<byte> Decompress_Huffman(byte* input, uint inSize);
 };
+
+#endif
