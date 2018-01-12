60f261d37e8e46364b906c719aaafac3085142be
puakai <puakai2010@gmail.com>
2018-01-12 19:53:45 +0900

    water fix ene

--- OpenGl1_Editor/src/Water.cpp
+++ OpenGl1_Editor/src/Water.cpp
@@ -1,44 +1,55 @@
 #include "Water.h"
+#include "Editor.h"
 
 void WaterParticle::repos() {
-	position += velocity * dt + force * dt * dt * 0.5f;
+	auto dp = velocity * dt + (force / (is_oxygen? MASS_O : MASS_H)) * dt * dt * 0.5;
+	position = position + dp;
 }
 
 void WaterParticle::f_bond() {
-	Vec3 fold = force;
+	dVec3 fold = dVec3(force);
 
 	auto other1 = Water::Get(p1);
 	auto other2 = Water::Get(p2);
 
-	Vec3 dir = other1->position - position;
-	force = K_LINEAR * (Normalize(dir) * (glm::length(dir) - BOND_LENGTH));
+	dVec3 dir = other1->position - position;
+	double dst = glm::length(dir);
+	force = K_LINEAR * Normalize(dir) * (dst - BOND_LENGTH);
 	if (is_oxygen) {
-		Vec3 dir = other2->position - position;
-		force += K_LINEAR * (Normalize(dir) * (glm::length(dir) - BOND_LENGTH));
+		dVec3 dir2 = other2->position - position;
+		double dst2 = glm::length(dir2);
+		force += K_LINEAR * Normalize(dir2) * (dst2 - BOND_LENGTH);
 
-		Vec3 dir1 = other1->position - position;
-		Vec3 dir2 = other2->position - position;
-		//H <--dir1-- me --dir2--> H
-		Vec3 t1 = Normalize(glm::cross(dir2, dir1));
+		//H <--dir-- me --dir2--> H
+		dVec3 t1 = Normalize(glm::cross(dir2, dir));
 
-		Vec3 rdir1 = Normalize(glm::cross(dir1, t1));
-		Vec3 rdir2 = Normalize(glm::cross(t1, dir2));
+		dVec3 rdir1 = Normalize(glm::cross(dir, t1));
+		dVec3 rdir2 = Normalize(glm::cross(t1, dir2));
 
-		float angle = acos(glm::dot(dir1, dir2));
-		force -= (rdir1 + rdir2)*K_RADIAL*(angle - BOND_ANGLE);
+		double angle = acos(Clamp(glm::dot(Normalize(dir), Normalize(dir2)), -1.0, 1.0));
+		force -= (rdir1/dst + rdir2/dst2)*K_RADIAL*(angle - BOND_ANGLE);
+
+		velocity += (fold + force) * 0.5 * dt / MASS_O;
+		
+		energy = 0.5 * K_RADIAL * pow((angle - BOND_ANGLE), 2);
+		energy += 0.5 * MASS_O * glm::dot(velocity, velocity);
 	}
 	else {
-		Vec3 dir1 = position - other1->position;
-		Vec3 dir2 = other2->position - other1->position;
+		dVec3 dir1 = -dir;
+		dVec3 dir2 = other2->position - other1->position;
 		//me <--dir1-- O --dir2--> H
-		Vec3 t1 = Normalize(glm::cross(dir2, dir1));
-		Vec3 rdir = Normalize(glm::cross(dir1, t1));
+		dVec3 t1 = Normalize(glm::cross(dir2, dir1));
+		dVec3 rdir = Normalize(glm::cross(dir1, t1));
 
-		float angle = acos(Clamp(glm::dot(Normalize(dir1), Normalize(dir2)), -1.0f, 1.0f));
-		force += rdir*K_RADIAL*(angle - BOND_ANGLE);
+		double angle = acos(Clamp(glm::dot(Normalize(dir1), Normalize(dir2)), -1.0, 1.0));
+		force += (rdir/dst)*K_RADIAL*(angle - BOND_ANGLE);
+
+		auto dv = (fold + force) * 0.5 * dt / MASS_H;
+		velocity = velocity + dv;
+
+		energy = 0.5 * K_LINEAR * pow(dst - BOND_LENGTH, 2);
+		energy += 0.5 * MASS_H * glm::dot(velocity, velocity);
 	}
-
-	velocity += (fold + force) * dt / (is_oxygen ? MASS_O : MASS_H);
 }
 
 
@@ -56,12 +67,12 @@ void Water::Init() {
 	//h1
 	me->particles[0]->p1 = 2;
 	me->particles[0]->p2 = 1;
-	me->particles[0]->position = Vec3(_ast * 0.5f, -_ast, 0);
+	me->particles[0]->position = dVec3(_ast * 1.05f, 0, 0);
 	me->particles[2]->is_oxygen = false;
 	//h2
 	me->particles[1]->p1 = 2;
 	me->particles[1]->p2 = 0;
-	me->particles[1]->position = Vec3(_ast * 0.5f, _ast, 0);
+	me->particles[1]->position = dVec3(0, _ast*1.1f, 0);
 	me->particles[2]->is_oxygen = false;
 	//o
 	me->particles[2]->p1 = 0;
@@ -69,12 +80,20 @@ void Water::Init() {
 	me->particles[2]->is_oxygen = true;
 }
 
+double ta = 0, to = 0;
+
 void Water::Update() {
+	to = ta;
+	ta = 0;
 	for (auto& a : me->particles) {
 		a->repos();
 	}
 	for (auto& a : me->particles) {
 		a->f_bond();
-		Engine::DrawQuad(200 + a->position.x * 1e12f, 200 + a->position.y * 1e12f, 5, 5, white());
+		ta += a->energy;
+		Engine::DrawQuad(Display::width*0.5f + (float)a->position.x * 1000, Display::height*0.5f + (float)a->position.y * 1000, 5, 5, white());
 	}
+
+	UI::Label(10, 10, 20, "Energy (J/mol):" + to_string(ta), Editor::instance->font, white());
+	UI::Label(10, 40, 20, "Energy change (d(J/mol)/dstep):" + to_string((ta-to)*Time::delta), Editor::instance->font, white());
 }
--- OpenGl1_Editor/src/Water.h
+++ OpenGl1_Editor/src/Water.h
@@ -1,25 +1,32 @@
 #pragma once
 #include "Engine.h"
 
-const float _ast = 1e-10f;
+typedef glm::highp_dvec3 dVec3;
 
-const float BOND_LENGTH = 1 * _ast;
-const float BOND_ANGLE = 109.47f * deg2rad;
-const float K_LINEAR = 345000.0e18f;
-const float K_RADIAL = 383.0e9f;
-const float dt = 5e-16f;
+const double _si_l = 1e-9;
+const double _si_m = 1.66e-27;
+const double _si_t = 1e-12;
+const double _si_q = 1.602e-19;
 
-const float avogadro = 6.02e23f;
-const float MASS_H = 1.0e-3f;// / avogadro;
-const float MASS_O = 16.0e-3f;// / avogadro;
+const double _ast = 0.1;
+
+const double BOND_LENGTH = 1 * _ast;
+const double BOND_ANGLE = 109.47 * deg2rad;
+const double K_LINEAR = 345000; //kJ / mol nm^2
+const double K_RADIAL = 383; //kJ / mol rad^2
+const double dt = 1e-4; //0.1fs
+
+const double MASS_H = 1.0;
+const double MASS_O = 15.99;
 
 class WaterParticle {
 public:
 	bool is_oxygen;
 
 	float charge;
-	Vec3 position, velocity, force;
+	dVec3 position, velocity, force;
 	uint p1, p2; //h1, h2 if oxygen, o, h2 if hydrogen
+	double energy;
 
 	void repos(), f_bond();
 };
